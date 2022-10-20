7b90e2311bcc5156ef8ad4be5b90590198810296
farhanacsebd <farhana.cse.bd@gmail.com>
2022-10-20 22:13:53 +0600

    added sweet alert

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 document.getElementById('btn').addEventListener('click',function(){
     const input = document.getElementById('input');
     navigator.clipboard.writeText(input.value);
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'copied',
+        showConfirmButton: false,
+        timer: 1500
+      })
 })
